62a4ecb276aca4842ed8ef829f1e93b011497d12
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-03-14 19:28:49 -0400

    Add header link names and styling

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,12 +1,18 @@
-
 import { Route } from "react-router-dom";
 import "./Home.css";
 
-export const Home:React.FC<any> = () => {
+export const Home: React.FC<any> = () => {
   return (
     <div className="Home">
-      <div className="Left-Header">I am the left header</div>
-      <div className="Right-Header">I am the right header</div>
+      <div className="Header">
+        <h3>Home</h3>
+        <h3>About</h3>
+        <h3>placeholder</h3>
+        <h3>Profile</h3>
+        <h3>Support</h3>
+      </div>
+      <div className="Welcome">Hey Fam!</div>
+      <div className="Description">I am the right header</div>
     </div>
   );
 };
