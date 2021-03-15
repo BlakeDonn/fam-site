a9f74968c2b704c0221ff3b7288adabfffd6c7b7
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-03-14 20:12:08 -0400

    Add firebase config

---
+++ src/Helpers/firebase.tsx
@@ -0,0 +1,26 @@
+
+// These samples are intended for Web so this import would normally be
+// done in HTML however using modules here is more convenient for
+// ensuring sample correctness offline.
+import firebase from "firebase/app";
+import "firebase/auth";
+
+export const firebaseInit = () => {
+ const firebaseConfig = {
+    apiKey: "AIzaSyAQmfS69dMcIqXv--f8slkzyTtqTkGSdbU",
+    authDomain: "famsite-85f71.firebaseapp.com",
+    projectId: "famsite-85f71",
+    storageBucket: "famsite-85f71.appspot.com",
+    messagingSenderId: "166086443232",
+    appId: "1:166086443232:web:b25e2df9e9e13a97579085",
+    measurementId: "G-B92YK9Q5Z2"
+  };
+  firebase.initializeApp(firebaseConfig);
+};
+
+export const logOut = ()=>{
+   firebase.auth().signOut()
+   localStorage.removeItem("displayName")
+   localStorage.removeItem("email")
+   localStorage.removeItem("userUID")
+}
