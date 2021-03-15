4f6e3d850c0ac934f7d563a41ed3037ba6a25939
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-03-15 15:27:50 -0400

    Adjust structuring for component modularity

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,25 +1,19 @@
 import { Route } from "react-router-dom";
+import { Login } from "../Login/Login";
+import { Header } from "../Header/Header";
 import "./Home.css";
 
 export const Home: React.FC<any> = () => {
   return (
     <div className="Home">
-      <div className="Header">
-        <h3>Home</h3>
-        <h3>About</h3>
-        <h3>Placeholder</h3>
-        <h3>Profile</h3>
-        <h3>Support</h3>
-      </div>
+      <Header />
       <div className="Welcome">
         <h1 className="Message">Hey Fam!</h1>
       </div>
       <div className="Description">
         <article>The Family Site for THE famliy</article>
       </div>
-      <div className="Login-form">
-        <article>The Family Site for THE famliy</article>
-      </div>
+      <Login />
     </div>
   );
 };
