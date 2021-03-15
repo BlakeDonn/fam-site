5d8dc805556e2d813b42dc887181f0be30654a33
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-03-15 15:29:00 -0400

    Make seperate component for modularity

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+export const Header: React.FC<any> = () => {
+  return (
+      <div className="Header">
+        <a>Home</a>
+        <a>About</a>
+        <a>Placeholder</a>
+        <a>Profile</a>
+        <a>Support</a>
+      </div>
+  );
+};
